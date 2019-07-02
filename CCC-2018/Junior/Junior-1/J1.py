a893f44a7ed5b0c7e6e36dbd0db6309a9cfbfe1a
Charles Chen <30297303+charlescchen@users.noreply.github.com>
2019-07-02 10:56:47 -0400

    Add Python J1 Solution

---
+++ CCC-2018/Junior/Junior-1/J1.py
@@ -0,0 +1,15 @@
+# CCC 2018 Junior 1: Telemarketer or not?
+#
+# Author: Charles Chen
+#
+# Simple if statement
+
+digit1 = int(input())
+digit2 = int(input())
+digit3 = int(input())
+digit4 = int(input())
+
+if (digit1 == 8 or digit1 == 9) and (digit2 == digit3) and (digit4 == 8 or digit4 == 9):
+    print("ignore")
+else:
+    print("answer")
